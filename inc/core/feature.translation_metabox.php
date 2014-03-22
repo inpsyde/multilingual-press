e2a3cfdcd49f1a3826bdfe77fe0f9cf363be926e
toscho <info@toscho.de>
2014-03-22 01:45:43 +0100

    update feature files

--- inc/core/feature.translation_metabox.php
+++ inc/core/feature.translation_metabox.php
@@ -1,7 +1,11 @@
 <?php # -*- coding: utf-8 -*-
 add_action( 'inpsyde_mlp_loaded', 'mlp_feature_translation_metabox' );
 
+/**
+ * @param Inpsyde_Property_List_Interface $data
+ * @return void
+ */
 function mlp_feature_translation_metabox( Inpsyde_Property_List_Interface $data ) {
 
-	$box = new Mlp_Translation_Metabox( $data );
+	new Mlp_Translation_Metabox( $data );
 }
