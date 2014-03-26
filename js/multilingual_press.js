1ef45d28374494a7db356da2f52689d3165650b5
toscho <info@toscho.de>
2014-03-26 18:05:17 +0100

    clean up main JS file

--- js/multilingual_press.js
+++ js/multilingual_press.js
@@ -1,131 +1,42 @@
 /**
  * jQuery Library for Multilingual Press
- * 
- * @author		fb, rw, ms, th
- * @version		0.8
+ *
+ * @version        2014.03.24
  * @package		mlp
- * @subpackage	jquery lib
- * 
+ *
  * @todo		Doc
  */
 
 jQuery.noConflict();
 ( function( $ ) {
-	
+
 	/**
 	 * Class Holder
 	 */
 	multilingual_press = {
-		
+
 		/**
 		 * Initial Function
 		 */
 		init : function () {
 			this.do_blog_checkup();
-			this.draw_tab();
-			this.bind_ajax_tab_handler();
-			this.bind_submit_setting_form();
-			this.form_fields_expandable();
+			this.set_toggle();
 		},
-		
-		/**
-		 * 
-		 */
-		bind_submit_setting_form : function() {
-			
-			$( '#multilingualpress_settings' ).live( 'submit', function() {
-				// Serialize form field/data
-				var serialized_data = $( this ).serialize();
-				multilingual_press.submit_form( serialized_data );
-				return false;
-			} );
-		},
-		
-		/**
-		 * 
-		 * @param serialized_data
-		 */
-		submit_form : function( serialized_data ) {
-			
-			var multilang_post_data = {
-				action: 'save_multilang_settings',
-				serialized_data: serialized_data + '&id=' + mlp_loc.blog_id,
-				form_nonce: mlp_loc.ajax_form_nonce,
-				id: mlp_loc.blog_id
-			};
-			
-			var multilang_saved_settings = $.ajax( {
-				url: ajaxurl,
-				data: multilang_post_data,
-				async: false,
-				type: 'POST',
-				success: function ( response ) {
-					$( '.wrap > .updated' ).remove();
-					return response;
-				}
-			} ).responseText;
-			
-			$( '#multilingualpress_settings' ).before( multilang_saved_settings );
-			window.scrollTo(0,0);
-		},
-		
-		/**
-		 * 
-		 */
-		draw_tab: function() {
-			// $( 'h3.nav-tab-wrapper:first' ).append( '<a id="mlp_settings_tab" class="nav-tab" href="#">' + mlp_loc.tab_label + '</a>' );
-		},
-		
-		/**
-		 * 
-		 */
-		bind_ajax_tab_handler : function() {
-			
-			$( 'a#mlp_settings_tab' ).live( 'click', function() {
-				
-				// Set tab active
-				$( 'h3.nav-tab-wrapper:first a' ).each( function() {
-					$( this ).removeClass( 'nav-tab-active' );
-				} );
-				$( this ).addClass( 'nav-tab-active' );
-								
-				var data = {
-					action: 'tab_form',
-					id: mlp_loc.blog_id,
-					tab_nonce: mlp_loc.ajax_tab_nonce
-				};
 
-				// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-				$.post( ajaxurl, data, function( response ) {
-					// Replace content
-					$( '.wrap:first' ).html( response );
-				} );
-			} );
-		},
-		
-		/**
-		 * 
-		 */
-		form_fields_expandable : function() {
-			$( 'form#multilingualpress_settings .handlediv' ).live( 'click', function() {
-				$( this ).parent( '.postbox' ).toggleClass( 'closed' );
-			} );
-		},
-		
 		/**
 		 * Retrieves all responses from the blog checkup
-		 * 
+		 *
 		 * @since	0.8
 		 */
 		do_blog_checkup : function() {
 			if ( ! '#multilingual_press_checkup' )
 				return;
-			
+
 			$( '#multilingual_press_checkup_link' ).live( 'click', function() {
 				var data = {
 					action: 'checkup_blogs'
 				};
-				
+
 				$.ajax( {
     				url: ajaxurl,
     				data: data,
@@ -133,19 +44,60 @@ jQuery.noConflict();
     				success: function ( response ) {
     					$( '#multilingual_press_checkup' ).append( response ).delay( 250 ).animate( {
     						backgroundColor: 'lightYellow',
-    						borderBottomColor: '#E6DB55', 
-    						borderLeftColor: '#E6DB55',
+						    borderBottomColor: '#E6DB55',
+						    borderLeftColor: '#E6DB55',
     						borderRightColor: '#E6DB55',
     						borderTopColor: '#E6DB55'
     					}, 500 ).delay( 3500 ).slideUp();
     				}
     			} );
 			} );
+		},
+
+		/**
+		 * Toggle handler, show/hide elements with the class 'mlp_toggler'.
+		 */
+		set_toggle: function () {
+
+			$(document).on("click", ".mlp_toggler", function (event) {
+
+				var toggle_container = $($(this).data('toggle_selector'));
+
+				// buttons and links
+				if ('submit' == this.type || 'A' == this.tagName) {
+					event.stopPropagation();
+					event.preventDefault();
+					toggle_container.toggle();
+					return false;
+				}
+
+				// labels, needs improvements
+				if ('LABEL' == this.tagName) {
+					var target = $('#' + $(this).attr('for'));
+
+					/* probably not necessary or useful
+					 if ( 'radio' != target.attr( 'type' ) )
+					 return true;*/
+
+					event.stopPropagation();
+
+					$('input[name="' + target.attr('name') + '"]').change(function () {
+						toggle_container.toggle(target.val() == $(this).val());
+						return true;
+					});
+
+					return true;
+				}
+
+				toggle_container.toggle();
+
+				return false;
+			});
 		}
 	};
-	
+
 	$( document ).ready( function( $ ) {
 		multilingual_press.init();
 	} );
-	
+
 } )( jQuery );
