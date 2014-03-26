b8a4d1a4883a3e4d9bceb9f129ef924e4db7e0b1
toscho <info@toscho.de>
2014-03-26 17:57:37 +0100

    version bump 2.0.0 RC3

--- multilingual-press.php
+++ multilingual-press.php
@@ -5,7 +5,7 @@
  * Description: Run WordPress Multisite with multiple languages.
  * Author:      Inpsyde GmbH
  * Author URI:  http://inpsyde.com
- * Version:     2.0.0-RC2
+ * Version:     2.0.0-RC3
  * Text Domain: multilingualpress
  * Domain Path: /languages
  * License:     GPLv3
