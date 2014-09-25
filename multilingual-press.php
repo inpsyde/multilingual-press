fd508ff240da3fda43eb43f9991c0fd8e396b92b
toscho <info@toscho.de>
2014-09-25 16:31:05 +0200

    Update main plugin file

--- multilingual-press.php
+++ multilingual-press.php
@@ -5,7 +5,7 @@
  * Description: Run WordPress Multisite with multiple languages.
  * Author:      Inpsyde GmbH
  * Author URI:  http://inpsyde.com
- * Version:     2.0.3
+ * Version:     2.1.0
  * Text Domain: multilingualpress
  * Domain Path: /languages
  * License:     GPLv3
@@ -19,7 +19,10 @@ if ( ! class_exists( 'Multilingual_Press' ) )
 // Kick-Off
 add_action( 'plugins_loaded', 'mlp_init', 0 );
 
+
 function mlp_init() {
+
+	global $wp_version, $wpdb, $pagenow;
 
 	$path   = plugin_dir_path( __FILE__ );
 
@@ -27,7 +30,6 @@ function mlp_init() {
 		require $path . 'inc/autoload/Mlp_Load_Controller.php';
 
 	$loader = new Mlp_Load_Controller( $path . 'inc' );
-
 	$data   = new Inpsyde_Property_List;
 
 	$data->loader           = $loader->get_loader();
@@ -38,6 +40,7 @@ function mlp_init() {
 	$data->css_url          = "{$data->plugin_url}css/";
 	$data->js_url           = "{$data->plugin_url}js/";
 	$data->image_url        = "{$data->plugin_url}images/";
+	$data->flag_url         = "{$data->plugin_url}flags/";
 
 	$headers = get_file_data(
 		__FILE__,
@@ -52,5 +55,83 @@ function mlp_init() {
 	foreach ( $headers as $name => $value )
 		$data->$name = $value;
 
-	new Multilingual_Press( $data );
+	if ( ! mlp_pre_run_test( $pagenow, $data, $wp_version, $wpdb ) )
+		return;
+
+	$mlp = new Multilingual_Press( $data, $wpdb );
+	$mlp->setup();
 }
+
+/**
+ * Check current state of the WordPress installation.
+ *
+ * @param  string                          $pagenow
+ * @param  Inpsyde_Property_List_Interface $data
+ * @param  string                          $wp_version
+ * @param  wpdb                            $wpdb
+ * @return bool
+ */
+function mlp_pre_run_test( $pagenow, Inpsyde_Property_List_Interface $data, $wp_version, wpdb $wpdb ) {
+
+	$self_check         = new Mlp_Self_Check( __FILE__, $pagenow );
+	$requirements_check = $self_check->pre_install_check(
+		 $data->plugin_name,
+		 $data->plugin_base_name,
+		 $wp_version
+	);
+
+	if ( Mlp_Self_Check::PLUGIN_DEACTIVATED === $requirements_check )
+		return FALSE;
+
+	$data->site_relations = new Mlp_Site_Relations( $wpdb, 'mlp_site_relations' );
+
+	if ( Mlp_Self_Check::INSTALLATION_CONTEXT_OK === $requirements_check ) {
+
+		$deactivator = new Mlp_Network_Plugin_Deactivation();
+
+		if ( 'MultilingualPress Pro' === $data->plugin_name ) {
+			$deactivator->deactivate( // remove the free version
+						array ( 'multilingual-press/multilingual-press.php' )
+			);
+		}
+
+		$last_version_option = get_site_option( 'mlp_version' );
+		$last_version        = new Mlp_Semantic_Version_Number( $last_version_option );
+		$current_version     = new Mlp_Semantic_Version_Number( $data->version );
+		$upgrade_check       = $self_check->is_current_version( $current_version, $last_version );
+		$updater             = new Mlp_Update_Plugin_Data( $data, $wpdb, $current_version, $last_version );
+
+		if ( Mlp_Self_Check::NEEDS_INSTALLATION === $upgrade_check )
+			$updater->install_plugin();
+
+		if ( Mlp_Self_Check::NEEDS_UPGRADE === $upgrade_check )
+			$updater->update( $deactivator );
+	}
+
+	return TRUE;
+}
+
+
+/**
+ * Write debug data to the error log.
+ *
+ * Add the following linge to your `wp-config.php` to enable this function:
+ *
+ *     const MULTILINGUALPRESS_DEBUG = TRUE;
+ *
+ * @param  string $message
+ * @return void
+ */
+function mlp_debug( $message ) {
+
+	if ( ! defined( 'MULTILINGUALPRESS_DEBUG' ) || ! MULTILINGUALPRESS_DEBUG )
+		return;
+
+	$date = date( 'H:m:s' );
+
+	error_log( "MultilingualPress: $date $message" );
+}
+
+
+if ( defined( 'MULTILINGUALPRESS_DEBUG' ) && MULTILINGUALPRESS_DEBUG )
+	add_action( 'mlp_debug', 'mlp_debug' );
