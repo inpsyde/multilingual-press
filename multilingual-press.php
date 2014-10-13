58301458b1ee4c2ffb7bb32ade07c3a0554594ae
toscho <info@toscho.de>
2014-10-13 12:23:07 +0200

    Update version number

--- multilingual-press.php
+++ multilingual-press.php
@@ -5,7 +5,7 @@
  * Description: Run WordPress Multisite with multiple languages.
  * Author:      Inpsyde GmbH
  * Author URI:  http://inpsyde.com
- * Version:     2.1.1
+ * Version:     2.1.2
  * Text Domain: multilingualpress
  * Domain Path: /languages
  * License:     GPLv3
