165d663eea898ab527e06a04b62fa3278a49eb54
tfrommen <tf@ipm-frommen.de>
2015-03-05 17:19:45 +0100

    Improve User Backend Language. Use current locale in case the current user has not yet been set. Close #88.

--- inc/user-backend-language/Mlp_User_Backend_Language.php
+++ inc/user-backend-language/Mlp_User_Backend_Language.php
@@ -1,10 +1,11 @@
-<?php
+<?php # -*- coding: utf-8 -*-
+
 /**
  * Class Mlp_User_Backend_Language
  *
- * Allow users to select an existing language for the entire network back end.
+ * Allow users to select an existing language for the entire network backend.
  *
- * @version 2014.03.18
+ * @version 2015.03.05
  * @author  Inpsyde GmbH
  * @license GPL
  */
@@ -39,38 +40,45 @@ class Mlp_User_Backend_Language {
 		$this->module_manager = $module_manager;
 	}
 
+	/**
+	 * Set up filters, when active.
+	 *
+	 * @return void
+	 */
 	public function setup() {
 
 		// Load user specific language in the backend
-		if ( is_admin() )
+		if ( is_admin() ) {
 			add_filter( 'locale', array( $this, 'locale' ) );
+		}
 
 		$this->active = $this->module_manager->register(
-			array (
+			array(
 				'display_name_callback' => array( $this, 'get_module_title' ),
-				'slug'			        => 'class-' . __CLASS__,
+				'slug'                  => 'class-' . __CLASS__,
 				'description_callback'  => array( $this, 'get_module_description' ),
 			)
 		);
 
 		// Quit here if module is turned off
-		if ( ! $this->active )
+		if ( ! $this->active ) {
 			return;
+		}
 
 		// Add User Field for own blog language
-		add_filter( 'personal_options', array ( $this, 'edit_user_profile' ) );
-		add_filter( 'profile_update',   array ( $this, 'profile_update' ) );
-
+		add_filter( 'personal_options', array( $this, 'edit_user_profile' ) );
+		add_filter( 'profile_update', array( $this, 'profile_update' ) );
 	}
 
 	/**
 	 * Get the description for this feature.
 	 *
-	 * Used in wp-admin/network/settings.php?page=mlp
+	 * Used in wp-admin/network/settings.php?page=mlp.
 	 *
 	 * @return string
 	 */
 	public function get_module_description() {
+
 		return __(
 			'Let each user choose a preferred language for the backend of all connected sites. Does not affect the frontend.',
 			'multilingualpress'
@@ -80,11 +88,12 @@ class Mlp_User_Backend_Language {
 	/**
 	 * Get the title for this feature.
 	 *
-	 * Used in wp-admin/network/settings.php?page=mlp
+	 * Used in wp-admin/network/settings.php?page=mlp.
 	 *
 	 * @return string
 	 */
 	public function get_module_title() {
+
 		return __( 'User Backend Language', 'multilingualpress' );
 	}
 
@@ -92,27 +101,28 @@ class Mlp_User_Backend_Language {
 	 * Display user meta.
 	 *
 	 * @wp-hook personal_options
-	 * @param   WP_User $user
-	 * @return	void
+	 *
+	 * @param WP_User $user User object.
+	 *
+	 * @return void
 	 */
 	public function edit_user_profile( WP_User $user ) {
 
 		$languages = get_available_languages();
 
-		if ( empty ( $languages ) )
+		if ( ! $languages ) {
 			return;
-
-		$user_language = $this->get_user_language( $user->data->ID );
+		}
 
 		// Add English manually, because it won't get added by WordPress itself.
-		$languages[] = 'en_US';
+		$languages[ ] = 'en_US';
 
+		$user_language = $this->get_user_language( $user->ID );
 		?>
 		<tr>
 			<th>
-				<label for="<?php echo $this->key; ?>"><?php
-					esc_html_e( 'Your preferred backend language', 'multilingualpress' );
-					?>
+				<label for="<?php echo $this->key; ?>">
+					<?php esc_html_e( 'Your preferred backend language', 'multilingualpress' ); ?>
 				</label>
 			</th>
 			<td>
@@ -121,85 +131,105 @@ class Mlp_User_Backend_Language {
 				</select>
 			</td>
 		</tr>
-		<?php
+	<?php
 	}
 
 	/**
 	 * Save the user meta.
 	 *
 	 * @wp-hook profile_update
-	 * @param	int $user_id The user id
-	 * @return	bool
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return bool
 	 */
 	public function profile_update( $user_id ) {
 
 		// Empty means, that the site language is used
-		if ( empty ( $_POST[ $this->key ] ) or '' === trim( $_POST[ $this->key ] ) )
+		if ( empty( $_POST[ $this->key ] ) or '' === trim( $_POST[ $this->key ] ) ) {
 			return delete_user_meta( $user_id, $this->key );
+		}
 
 		return update_user_meta( $user_id, $this->key, $_POST[ $this->key ] );
-
 	}
 
 	/**
 	 * Gets the language which the user set up in his profile.
 	 *
-	 * @param	int $user_id the id of the user
-	 * @param   string $default
-	 * @return	string $user_language the users preferred language
+	 * @param int    $user_id The user ID.
+	 * @param string $default Optional. Default language. Defaults to ''.
+	 *
+	 * @return string $user_language The user's preferred language.
 	 */
 	public function get_user_language( $user_id, $default = '' ) {
 
 		$setting = get_user_meta( $user_id, $this->key, TRUE );
 
-		if ( empty ( $setting ) )
+		if ( empty( $setting ) ) {
 			return $default;
+		}
 
 		return $setting;
 	}
 
- 	/**
+	/**
 	 * Get the language of the blog in the admin area.
 	 *
 	 * @wp-hook locale
-	 * @param   string $locale
-	 * @return	string
+	 *
+	 * @param string $locale
+	 *
+	 * @return string
 	 */
 	public function locale( $locale ) {
 
-		if ( ! $this->active )
+		if ( ! $this->active ) {
 			return $locale;
+		}
 
-		return $this->get_user_language( get_current_user_id(), $locale );
+		if ( ! did_action( 'set_current_user' ) ) {
+			return $locale;
+		}
+
+		$current_user_id = get_current_user_id();
+
+		return $this->get_user_language( $current_user_id, $locale );
 	}
 
 	/**
 	 * Show language selector.
 	 *
-	 * @param array  $lang_files
-	 * @param string $current
+	 * @param array  $lang_files Optional. Language file names. Defaults to array().
+	 * @param string $current    Optional. Current language code. Defaults to ''.
+	 *
 	 * @return void
 	 */
-	private function dropdown_languages( $lang_files = array(), $current = '' ) {
+	private function dropdown_languages( array $lang_files = array(), $current = '' ) {
 
 		$output = array();
 
 		// Inherit site specific language
-		$output[] = '<option value=""' . selected( $current, '', false ) . '>' . __( 'Site Language' ) . "</option>";
+		$output[ ] = '<option value=""' . selected( $current, '', FALSE ) . '>' . __( 'Site Language' ) . "</option>";
 
 		foreach ( (array) $lang_files as $file_name ) {
-
 			$code_lang = basename( $file_name, '.mo' );
 			$code_lang = esc_attr( $code_lang );
 
-			if ( 'en_US' === $code_lang )
-				$lang = __( 'English' ); // American English
-			else
+			if ( 'en_US' === $code_lang ) {
+				// American English
+				$lang = __( 'English' );
+			} else {
 				$lang = format_code_lang( $code_lang );
+			}
 
-			$lang          = esc_html ( $lang );
-			$selected      = selected( $current, $code_lang, FALSE );
-			$output[$lang] = "<option value='$code_lang' $selected>$lang</option>";
+			$lang = esc_html( $lang );
+
+			$selected = selected( $code_lang, $current, FALSE );
+			if ( '' !== $selected ) {
+				$selected = ' ' . $selected;
+			}
+
+			$output[ $lang ] = '<option value="' . $code_lang . '"' . $selected . '>' . $lang . '</option>';
 		}
 
 		// Order by name
@@ -207,5 +237,5 @@ class Mlp_User_Backend_Language {
 
 		echo implode( "\n\t", $output );
 	}
-}
 
+}
