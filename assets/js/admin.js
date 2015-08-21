e5bc6d9c68025f369feb5343108b50f4b5d56419
tfrommen <tf@ipm-frommen.de>
2015-08-21 16:40:24 +0200

    Update admin JS and POT file.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -344,3 +344,53 @@
 	} );
 
 } )( jQuery, mlpRelationshipControlL10n );
+
+;( function( $ ) {
+	"use strict";
+
+	var term_translator = {
+
+		init: function() {
+			this.isPropagating = false;
+
+			this.propagate_term_selection();
+		},
+
+		propagate_term_selection: function() {
+			var $table = $( '.mlp_term_selections' );
+
+			if ( $table.length ) {
+				var $selects = $table.find( 'select' );
+
+				$table.on( 'change', 'select', function() {
+					if ( term_translator.isPropagating ) {
+						return;
+					}
+
+					term_translator.isPropagating = true;
+
+					var $this = $( this ),
+						relation = $this.find( '[value="' + $this.val() + '"]' ).data( 'relation' );
+
+					$selects.not( $this ).each( function() {
+						var $this = $( this ),
+							$option = $this.find( 'option[data-relation="' + relation + '"]' );
+
+						if ( $option.length ) {
+							$this.val( $option.val() );
+						} else {
+							$this.val( $this.find( 'option' ).first().val() );
+						}
+					} );
+
+					term_translator.isPropagating = false;
+				} );
+			}
+		}
+	};
+
+	$( function() {
+		term_translator.init();
+	} );
+
+} )( jQuery );
