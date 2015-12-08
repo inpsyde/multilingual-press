02ac0cbc1623cb94ce6c44ba2bdfe58b1f2ba17e
tfrommen <info@tfrommen.de>
2015-12-08 17:04:27 +0100

    Use a better fitting nonce action for the Redirect site setting.

--- src/inc/redirect/Mlp_Redirect_Site_Settings.php
+++ src/inc/redirect/Mlp_Redirect_Site_Settings.php
@@ -1,12 +1,7 @@
-<?php
+<?php # -*- coding: utf-8 -*-
+
 /**
- * Site specific settings.
- *
- * @version 2014.04.28
- * @author  Inpsyde GmbH, toscho
- * @license GPL
- * @package    MultilingualPress
- * @subpackage Redirect
+ * Handles the site-specific Redirect setting.
  */
 class Mlp_Redirect_Site_Settings {
 
@@ -21,6 +16,7 @@ class Mlp_Redirect_Site_Settings {
 	 * @param string $option_name
 	 */
 	public function __construct( $option_name ) {
+
 		$this->option_name = $option_name;
 	}
 
@@ -31,11 +27,14 @@ class Mlp_Redirect_Site_Settings {
 	 */
 	public function setup() {
 
-		$nonce = new Inpsyde_Nonce_Validator( 'mlp_redirect' );
-		$data  = new Mlp_Redirect_Settings_Data( $nonce, $this->option_name );
-		$view  = new Mlp_Redirect_Site_Settings_Form( $nonce, $data );
+		$nonce = new Inpsyde_Nonce_Validator( 'save_redirect_site_setting' );
 
-		add_filter( 'mlp_blogs_add_fields',  array ( $view, 'render' ) );
-		add_filter( 'mlp_blogs_save_fields', array ( $data, 'save' ) );
+		$data = new Mlp_Redirect_Settings_Data( $nonce, $this->option_name );
+
+		$view = new Mlp_Redirect_Site_Settings_Form( $nonce, $data );
+
+		add_filter( 'mlp_blogs_add_fields', array( $view, 'render' ) );
+
+		add_filter( 'mlp_blogs_save_fields', array( $data, 'save' ) );
 	}
 }
