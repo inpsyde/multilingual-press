3537a9d22ae5605cdcf48605f42599e7ae4355bb
tfrommen <info@tfrommen.de>
2015-12-08 16:55:01 +0100

    Use a better fitting nonce action for the Redirect user setting.

--- src/inc/redirect/Mlp_Redirect_User_Settings.php
+++ src/inc/redirect/Mlp_Redirect_User_Settings.php
@@ -6,26 +6,23 @@
 class Mlp_Redirect_User_Settings {
 
 	/**
-	 * @var string
-	 */
-	private $meta_key = 'mlp_redirect';
-
-	/**
 	 * Initializes the objects.
 	 *
 	 * @return void
 	 */
 	public function setup() {
 
-		$nonce = new Inpsyde_Nonce_Validator( $this->meta_key );
+		$meta_key = 'mlp_redirect';
+
+		$nonce = new Inpsyde_Nonce_Validator( 'save_redirect_user_setting' );
 
 		$user_settings_controller = new Mlp_User_Settings_Controller(
-			new Mlp_Redirect_User_Settings_Html( $this->meta_key, $nonce ),
-			new Mlp_User_Settings_Updater( $this->meta_key, $nonce )
+			new Mlp_Redirect_User_Settings_Html( $meta_key, $nonce ),
+			new Mlp_User_Settings_Updater( $meta_key, $nonce )
 		);
 		$user_settings_controller->setup();
 
-		$redirect_filter = new Mlp_Redirect_Filter( $this->meta_key );
+		$redirect_filter = new Mlp_Redirect_Filter( $meta_key );
 		add_filter( 'mlp_do_redirect', array( $redirect_filter, 'filter_redirect' ) );
 	}
 }
