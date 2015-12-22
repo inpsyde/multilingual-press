503da60dfdaa56f90f76bb2401c1e843d37b341a
tfrommen <info@tfrommen.de>
2015-12-22 13:35:10 +0100

    Remove obsolete JavaScript. #168

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -437,43 +437,6 @@
 	MultilingualPress.registerModule( 'options-general.php', 'UserBackendLanguage', UserBackendLanguage );
 })();
 
-;( function( $ ) {
-	"use strict";
-
-	var advanced_translator = {
-
-		init: function() {
-			this.meta_box_init();
-			this.meta_box_toggle_switch();
-		},
-
-		meta_box_init: function() {
-			$( '.to_translate' ).hide();
-
-			$( 'input.do_translate[checked]' ).each( function() {
-				var data = $( this ).attr( 'data' );
-
-				$( '.translate_' + data ).toggle();
-				$( '#content_' + data + '_ifr' ).height( 400 );
-			} );
-		},
-
-		meta_box_toggle_switch: function() {
-			$( '.do_translate' ).on( 'click', function() {
-				var data = $( this ).attr( 'data' );
-
-				$( '.translate_' + data ).toggle( 'slow' );
-				$( '#content_' + data + '_ifr' ).height( 400 );
-			} );
-		}
-	};
-
-	$( function() {
-		advanced_translator.init();
-	} );
-
-} )( jQuery );
-
 /* global ajaxurl, mlpRelationshipControlL10n */
 ;( function( $, mlpL10n ) {
 	"use strict";
