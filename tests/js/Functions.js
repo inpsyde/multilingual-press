916dac4df99c4e35719606239cb8f12637dda954
tfrommen <info@tfrommen.de>
2016-04-05 12:55:12 +0200

    Introduce test helper functions.

---
+++ tests/js/Functions.js
@@ -0,0 +1,31 @@
+export const returnArg = ( arg ) => {
+	return arg;
+};
+
+export const returnEmptyArray = () => {
+	return [];
+};
+
+export const returnEmptyObject = () => {
+	return {};
+};
+
+export const returnEmptyString = () => {
+	return '';
+};
+
+export const returnFalse = () => {
+	return false;
+};
+
+export const returnNull = () => {
+	return null;
+};
+
+export const returnTrue = () => {
+	return true;
+};
+
+export const returnUndefined = () => {
+	return undefined;
+};
