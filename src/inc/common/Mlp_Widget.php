2e66765e6916c193293e6df1eef9b9e846585c43
tfrommen <info@tfrommen.de>
2016-04-21 07:55:49 +0200

    Refactor and optimize Language Switcher widget.

--- src/inc/common/Mlp_Widget.php
+++ src/inc/common/Mlp_Widget.php
@@ -1,18 +1,9 @@
-<?php
+<?php # -*- coding: utf-8 -*-
 
 /**
- * Language switcher widget
- *
- * @author  Inpsyde GmbH, toscho
- * @version 2015.06.26
- * @license GPL
+ * Language Switcher widget.
  */
 class Mlp_Widget extends WP_Widget {
-
-	/**
-	 * @var string
-	 */
-	protected static $handle = 'mlp_widget';
 
 	/**
 	 * @var Mlp_Assets_Interface
@@ -20,21 +11,46 @@ class Mlp_Widget extends WP_Widget {
 	private static $assets;
 
 	/**
-	 * Register the widget and set up the description.
+	 * Constructor. Sets up the properties.
 	 */
 	public function __construct() {
 
-		add_action( 'template_redirect', array( $this, 'require_style' ) );
-
 		parent::__construct( 'Mlp_Widget', __( 'Language Switcher', 'multilingual-press' ), array(
-			'classname'                   => self::$handle,
+			'classname'                   => 'mlp_widget',
 			'description'                 => __( 'MultilingualPress Translations', 'multilingual-press' ),
 			'customize_selective_refresh' => true,
 		) );
+
+		// Enqueue style if widget is active (appears in a sidebar) or if in Customizer preview.
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'require_style' ) );
+		}
 	}
 
 	/**
-	 * Load frontend CSS if the widget is active.
+	 * Registers the widget.
+	 *
+	 * @return void
+	 */
+	public static function widget_register() {
+
+		register_widget( __CLASS__ );
+	}
+
+	/**
+	 * Sets the assets manager.
+	 *
+	 * @param Mlp_Assets_Interface $assets Assets.
+	 *
+	 * @return void
+	 */
+	public static function insert_asset_instance( Mlp_Assets_Interface $assets ) {
+
+		self::$assets = $assets;
+	}
+
+	/**
+	 * Enqueues the frontend styles.
 	 *
 	 * @wp-hook template_redirect
 	 *
@@ -42,24 +58,18 @@ class Mlp_Widget extends WP_Widget {
 	 */
 	public function require_style() {
 
-		if ( ! is_active_widget( false, false, self::$handle ) ) {
-			return false;
-		}
-
 		$theme_support = get_theme_support( 'multilingualpress' );
 		if ( ! empty( $theme_support[0]['language_switcher_widget_style'] ) ) {
 			return false;
 		}
 
-		self::$assets->provide( 'mlp_frontend_css' );
-
-		return true;
+		return self::$assets->provide( 'mlp_frontend_css' );
 	}
 
 	/**
-	 * Display widget admin form.
+	 * Renders the widget's admin view.
 	 *
-	 * @param array $instance Widget settings
+	 * @param array $instance Widget settings.
 	 *
 	 * @return void
 	 */
@@ -67,20 +77,20 @@ class Mlp_Widget extends WP_Widget {
 
 		$instance = $this->adapt_settings( $instance );
 
-		$title             = isset( $instance['widget_title'] )
-			? esc_attr( $instance['widget_title'] ) : '';
-		$link_type         = isset( $instance['widget_title'] )
-			? esc_attr( $instance['widget_link_type'] ) : '';
-		$show_current_blog = isset( $instance['widget_show_current_blog'] )
-			? strip_tags( $instance['widget_show_current_blog'] ) : '';
-		$display_flag      = isset( $instance['widget_display_flag'] )
-			? strip_tags( $instance['widget_display_flag'] ) : '';
-		$show_widget       = isset( $instance['widget_toggle_view_on_translated_posts'] )
-			? strip_tags( $instance['widget_toggle_view_on_translated_posts'] ) : '';
+		$title = isset( $instance['widget_title'] ) ? $instance['widget_title'] : '';
+
+		$link_type = isset( $instance['widget_link_type'] ) ? $instance['widget_link_type'] : '';
+
+		$display_flag = isset( $instance['widget_display_flag'] );
+
+		$show_current_blog = isset( $instance['widget_show_current_blog'] );
+
+		$show_widget = isset( $instance['widget_toggle_view_on_translated_posts'] );
 		?>
 		<p>
 			<?php
-			$id   = $this->get_field_id( 'mlp_widget_title' );
+			$id = $this->get_field_id( 'mlp_widget_title' );
+
 			$name = $this->get_field_name( 'mlp_widget_title' );
 			?>
 			<label for="<?php echo esc_attr( $id ); ?>"><?php _e( 'Title', 'multilingual-press' ); ?></label><br>
@@ -89,114 +99,114 @@ class Mlp_Widget extends WP_Widget {
 		</p>
 		<p>
 			<?php
-			$id   = $this->get_field_id( 'mlp_widget_link_type' );
+			$id = $this->get_field_id( 'mlp_widget_link_type' );
+
 			$name = $this->get_field_name( 'mlp_widget_link_type' );
+
+			$options = array(
+				'none'           => __( 'None', 'multilingual-press' ),
+				'native'         => __( 'Native name', 'multilingual-press' ),
+				'text'           => __( 'Custom name', 'multilingual-press' ),
+				'english'        => __( 'English name', 'multilingual-press' ),
+				'http'           => __( 'Language code', 'multilingual-press' ),
+				'language_short' => __( 'Language code (short)', 'multilingual-press' ),
+			);
 			?>
 			<label for="<?php echo esc_attr( $id ); ?>"><?php _e( 'Link text', 'multilingual-press' ); ?></label>
 			<select name="<?php echo esc_attr( $name ); ?>" class="widefat" id="<?php echo esc_attr( $id ); ?>"
 				autocomplete="off">
-				<?php
-				$options = array(
-					'none'           => __( 'None', 'multilingual-press' ),
-					'native'         => __( 'Native name', 'multilingual-press' ),
-					'text'           => __( 'Custom name', 'multilingual-press' ),
-					'english'        => __( 'English name', 'multilingual-press' ),
-					'http'           => __( 'Language code', 'multilingual-press' ),
-					'language_short' => __( 'Language code (short)', 'multilingual-press' ),
-				);
-				foreach ( $options as $value => $text ) {
-					printf(
-						'<option value="%1$s"%2$s>%3$s</option>',
-						esc_attr( $value ),
-						selected( $link_type, $value, false ),
-						esc_html( $text )
-					);
-				}
-				?>
+				<?php foreach ( $options as $value => $text ) : ?>
+					<option value="<?php echo esc_attr( $value ); ?>"<?php selected( $value, $link_type ); ?>>
+						<?php echo esc_html( $text ); ?>
+					</option>
+				<?php endforeach; ?>
 			</select>
 		</p>
 		<p>
 			<?php
-			$id   = $this->get_field_id( 'mlp_widget_display_flag' );
+			$id = $this->get_field_id( 'mlp_widget_display_flag' );
+
 			$name = $this->get_field_name( 'mlp_widget_display_flag' );
 			?>
 			<label for="<?php echo esc_attr( $id ); ?>">
-				<input type="checkbox"
-					name="<?php echo esc_attr( $name ); ?>" value="1" id="<?php echo esc_attr( $id ); ?>"
-					<?php checked( $display_flag, 1 ); ?>>
+				<input type="checkbox" name="<?php echo esc_attr( $name ); ?>" value="1"
+					id="<?php echo esc_attr( $id ); ?>"<?php checked( $display_flag ); ?>>
 				<?php _e( 'Show flag', 'multilingual-press' ); ?>
 			</label>
 		</p>
 		<p>
 			<?php
-			$id   = $this->get_field_id( 'mlp_widget_show_current_blog' );
+			$id = $this->get_field_id( 'mlp_widget_show_current_blog' );
+
 			$name = $this->get_field_name( 'mlp_widget_show_current_blog' );
 			?>
 			<label for="<?php echo esc_attr( $id ); ?>">
-				<input type="checkbox"
-					name="<?php echo esc_attr( $name ); ?>" value="1" id="<?php echo esc_attr( $id ); ?>"
-					<?php checked( $show_current_blog, 1 ); ?>>
+				<input type="checkbox" name="<?php echo esc_attr( $name ); ?>" value="1"
+					id="<?php echo esc_attr( $id ); ?>"<?php checked( $show_current_blog ); ?>>
 				<?php _e( 'Show current site', 'multilingual-press' ); ?>
 			</label>
 		</p>
 		<p>
 			<?php
-			$id   = $this->get_field_id( 'mlp_widget_toggle_view_on_translated_posts' );
+			$id = $this->get_field_id( 'mlp_widget_toggle_view_on_translated_posts' );
+
 			$name = $this->get_field_name( 'mlp_widget_toggle_view_on_translated_posts' );
 			?>
 			<label for="<?php echo esc_attr( $id ); ?>">
-				<input type="checkbox" name="<?php echo esc_attr( $name ); ?>" id="<?php echo esc_attr( $id ); ?>"
-					value="1"<?php checked( $show_widget, 1 ); ?>>
+				<input type="checkbox" name="<?php echo esc_attr( $name ); ?>" value="1"
+					id="<?php echo esc_attr( $id ); ?>"<?php checked( $show_widget ); ?>>
 				<?php _e( 'Show links for translated content only.', 'multilingual-press' ); ?>
 			</label>
 		</p>
 		<p>
-			<?php
-			if ( current_user_can( 'manage_network_options' ) ) {
-				echo sprintf(
+			<?php if ( current_user_can( 'manage_network_options' ) ) : ?>
+				<?php
+				printf(
 					__( 'Languages are sorted by <a href="%s">priority</a>.', 'multilingual-press' ),
 					network_admin_url( 'settings.php?page=language-manager' )
 				);
-			} else {
-				_e( 'Languages are sorted by priority.', 'multilingual-press' );
-			}
-			?>
+				?>
+			<?php else : ?>
+				<?php _e( 'Languages are sorted by priority.', 'multilingual-press' ); ?>
+			<?php endif; ?>
 		</p>
 		<?php
 	}
 
 	/**
-	 * Callback for widget update.
+	 * Updates the widget settings.
 	 *
 	 * @param array $new_instance New widget settings.
-	 * @param array $old_instance Widget settings.
+	 * @param array $instance     Current widget settings.
 	 *
 	 * @return array
 	 */
-	public function update( $new_instance, $old_instance ) {
+	public function update( $new_instance, $instance ) {
 
-		$instance = $old_instance;
+		$instance['widget_title'] = esc_html( $new_instance['mlp_widget_title'] );
 
-		$instance['widget_title']                           = esc_html( $new_instance['mlp_widget_title'] );
-		$instance['widget_link_type']                       = esc_attr( $new_instance['mlp_widget_link_type'] );
-		$instance['widget_show_current_blog']               = (int) (
-			isset( $new_instance['mlp_widget_show_current_blog'] )
-			&& $new_instance['mlp_widget_show_current_blog'] === '1'
-		);
-		$instance['widget_display_flag']                    = (int) (
+		$instance['widget_link_type'] = esc_attr( $new_instance['mlp_widget_link_type'] );
+
+		$instance['widget_display_flag'] = (int) (
 			isset( $new_instance['mlp_widget_display_flag'] )
-			&& $new_instance['mlp_widget_display_flag'] === '1'
+			&& '1' === $new_instance['mlp_widget_display_flag']
 		);
+
+		$instance['widget_show_current_blog'] = (int) (
+			isset( $new_instance['mlp_widget_show_current_blog'] )
+			&& '1' === $new_instance['mlp_widget_show_current_blog']
+		);
+
 		$instance['widget_toggle_view_on_translated_posts'] = (int) (
 			isset( $new_instance['mlp_widget_toggle_view_on_translated_posts'] )
-			&& $new_instance['mlp_widget_toggle_view_on_translated_posts'] === '1'
+			&& '1' === $new_instance['mlp_widget_toggle_view_on_translated_posts']
 		);
 
 		return $instance;
 	}
 
 	/**
-	 * Frontend display.
+	 * Renders the widget's front end view.
 	 *
 	 * When a widget is restored from trash, the instance might be incomplete, hence the preparations.
 	 *
@@ -209,33 +219,17 @@ class Mlp_Widget extends WP_Widget {
 
 		$instance = $this->adapt_settings( $instance );
 
-		$link_type = 'text';
-		if ( ! empty( $instance['widget_link_type'] ) ) {
-			$link_type = $instance['widget_link_type'];
-		}
-
-		$display_flag = false;
-		if ( ! empty( $instance['widget_display_flag'] ) ) {
-			$display_flag = $instance['widget_display_flag'];
-		}
-
-		$show_current = true;
-		if ( isset ( $instance['widget_show_current_blog'] ) ) {
-			$show_current = (int) $instance['widget_show_current_blog'] === 1;
-		}
-
-		$output_args = array(
-			'link_text'         => $link_type,
-			'show_current_blog' => $show_current,
-			'display_flag'      => $display_flag,
-		);
-		$output      = Mlp_Helpers::show_linked_elements( $output_args );
+		$output = Mlp_Helpers::show_linked_elements( array(
+			'link_text'         => empty( $instance['widget_link_type'] ) ? 'text' : $instance['widget_link_type'],
+			'show_current_blog' => ! empty( $instance['widget_show_current_blog'] ),
+			'display_flag'      => ! empty( $instance['widget_display_flag'] ),
+		) );
 		if ( ! $output ) {
 			return;
 		}
 
 		$title = '';
-		if ( isset( $instance['widget_title'] ) ) {
+		if ( ! empty( $instance['widget_title'] ) ) {
 			$title = $instance['widget_title'];
 		}
 		/** This filter is documented in wp-includes/default-widgets.php */
@@ -253,11 +247,11 @@ class Mlp_Widget extends WP_Widget {
 	}
 
 	/**
-	 * Adapt the internal settings to changes introduced in MultilingualPress 2.2.0.
+	 * Adapts the internal settings to changes introduced in MultilingualPress 2.2.0.
 	 *
 	 * @see  https://github.com/inpsyde/multilingual-press/issues/112
 	 *
-	 * @todo Eventually remove this, with version 2.6.0 at the earliest.
+	 * @todo Eventually remove this, with version 2.2.0 + 4 at the earliest.
 	 *
 	 * @param array $instance Widget settings.
 	 *
@@ -268,14 +262,14 @@ class Mlp_Widget extends WP_Widget {
 		$settings = $this->get_settings();
 
 		if ( empty( $settings[ $this->number ] ) ) {
-			// This should not happen (if it does, there's something wrong with WP_Widget)
+			// This should not happen (if it does, there's something wrong with WP_Widget).
 			return $instance;
 		}
 
 		$instance = $settings[ $this->number ];
 
 		if ( empty( $instance['widget_link_type'] ) ) {
-			// No need to adapt anything
+			// No need to adapt anything.
 			return $instance;
 		}
 
@@ -291,7 +285,7 @@ class Mlp_Widget extends WP_Widget {
 				break;
 
 			default:
-				// No need to adapt anything
+				// No need to adapt anything.
 				return $instance;
 		}
 
@@ -300,27 +294,5 @@ class Mlp_Widget extends WP_Widget {
 		$this->save_settings( $settings );
 
 		return $instance;
-	}
-
-	/**
-	 * Register the widget.
-	 *
-	 * @return void
-	 */
-	public static function widget_register() {
-
-		register_widget( __CLASS__ );
-	}
-
-	/**
-	 * Insert assets.
-	 *
-	 * @param Mlp_Assets_Interface $assets Assets.
-	 *
-	 * @return void
-	 */
-	public static function insert_asset_instance( Mlp_Assets_Interface $assets ) {
-
-		self::$assets = $assets;
 	}
 }
