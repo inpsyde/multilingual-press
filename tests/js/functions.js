6a2c1876a27e168556f173ee95f419b36482c819
tfrommen <info@tfrommen.de>
2016-04-22 22:23:37 +0200

    Add (and fix) helper functions.

--- tests/js/functions.js
+++ tests/js/functions.js
@@ -1,8 +1,12 @@
+export const getRandomInteger = ( min = 0, max = 10e14 - 1 ) => Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+
+export const getRandomString = ( length = 16 ) => Math.random().toString( 36 ).slice( 2 ).substr( 0, length );
+
 export const returnArg = ( arg ) => arg;
 
 export const returnEmptyArray = () => [];
 
-export const returnEmptyObject = () => {};
+export const returnEmptyObject = () => { return {}; };
 
 export const returnEmptyString = () => '';
 
