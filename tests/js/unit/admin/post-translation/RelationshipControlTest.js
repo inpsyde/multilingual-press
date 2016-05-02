3715e5c21864616ee46c3306ca9dc210d8c334e2
tfrommen <info@tfrommen.de>
2016-05-02 11:10:48 +0200

    Add more tests for RelationshipControl module.

--- tests/js/unit/admin/post-translation/RelationshipControlTest.js
+++ tests/js/unit/admin/post-translation/RelationshipControlTest.js
@@ -62,4 +62,110 @@ test( 'initializeEventHandlers ...', ( assert ) => {
 	assert.end();
 } );
 
-// TODO: More tests...
+// TODO: updateUnsavedRelationships
+
+// TODO: findMetaBox
+
+// TODO: confirmUnsavedRelationships
+
+// TODO: saveRelationship
+
+// TODO: getEventName
+
+test( 'connectNewPost ...', ( assert ) => {
+	const testee = createTestee();
+
+	// Turn method in to spy.
+	testee.sendRequest = sinon.spy();
+
+	const postTitle = F.getRandomString();
+
+	const $postTitle = new jQueryObject();
+	$postTitle.val.returns( postTitle );
+
+	$.withArgs( 'input[name="post_title"]' ).returns( $postTitle );
+
+	const data = F.getRandomObject();
+
+	testee.connectNewPost( data );
+
+	assert.equal(
+		testee.sendRequest.callCount,
+		1,
+		'... SHOULD call sendRequest().'
+	);
+
+	// Manipulate data object for subsequent test.
+	data.new_post_title = postTitle;
+
+	assert.deepEqual(
+		testee.sendRequest.firstCall.args[ 0 ],
+		data,
+		'... SHOULD pass the expected data to sendRequest().'
+	);
+
+	assert.end();
+} );
+
+test( 'disconnectPost ...', ( assert ) => {
+	const testee = createTestee();
+
+	// Turn method in to spy.
+	testee.sendRequest = sinon.spy();
+
+	const data = F.getRandomString();
+
+	testee.disconnectPost( data );
+
+	assert.equal(
+		testee.sendRequest.callCount,
+		1,
+		'... SHOULD call sendRequest().'
+	);
+
+	assert.equal(
+		testee.sendRequest.calledWith( data ),
+		true,
+		'... SHOULD pass the expected data to sendRequest().'
+	);
+
+	assert.end();
+} );
+
+// TODO: connectExistingPost
+
+test( 'sendRequest ...', ( assert ) => {
+	const Util = {
+		reloadLocation: F.getRandomString()
+	};
+
+	const testee = createTestee( { Util } );
+
+	const url = window.ajaxurl = F.getRandomString();
+
+	const data = F.getRandomString();
+
+	testee.sendRequest( data );
+
+	assert.equal(
+		$.ajax.callCount,
+		1,
+		'... SHOULD send an AJAX request.'
+	);
+
+	const ajaxData = {
+		async: false,
+		data,
+		success: Util.reloadLocation,
+		type: 'POST',
+		url
+	};
+
+	assert.deepEqual(
+		$.ajax.firstCall.args[ 0 ],
+		ajaxData,
+		'... SHOULD pass along the expected data.'
+	);
+
+	assert.end();
+} );
