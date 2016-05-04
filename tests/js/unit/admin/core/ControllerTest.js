2acbe4750ceba2fc3fd7b6d4f3e17120dbe0b879
tfrommen <info@tfrommen.de>
2016-05-04 10:58:18 +0200

    Add Controller test.

---
+++ tests/js/unit/admin/core/ControllerTest.js
@@ -0,0 +1,115 @@
+import globalStub from "../../stubs/global";
+import test from "tape";
+import sinon from "sinon";
+import * as F from "../../functions";
+import Backbone from "../../stubs/Backbone";
+import Controller from "../../../../../resources/js/admin/core/Controller";
+
+/**
+ * Returns a new instance of the class under test.
+ * @param {Registry} [registry] - Optional. The registry object. Defaults to a Sinon.JS stub.
+ * @param {Object} [settings={}] - Optional. The controller settings. Defaults to an empty object.
+ * @returns {Controller} The instance of the class under test.
+ */
+const createTestee = ( registry = sinon.stub(), settings = {} ) => {
+	// Rewire internal data.
+	Controller.__Rewire__( '_this', {} );
+
+	return new Controller( registry, settings );
+};
+
+test( 'settings ...', ( assert ) => {
+	const settings = F.getRandomString();
+
+	const testee = createTestee( null, settings );
+
+	assert.equal(
+		testee.settings,
+		settings,
+		'... SHOULD have the expected value.'
+	);
+
+	assert.end();
+} );
+
+test( 'initialize ...', ( assert ) => {
+	const modules = F.getRandomString();
+
+	const registry = {
+		initializeRoutes: sinon.stub().returns( modules )
+	};
+
+	const testee = createTestee( registry );
+
+	// Turn method into spy.
+	testee.maybeStartHistory = sinon.spy();
+
+	assert.equal(
+		testee.initialize(),
+		modules,
+		'... SHOULD return the expected modules.'
+	);
+
+	assert.equal(
+		testee.maybeStartHistory.callCount,
+		1,
+		'... SHOULD (maybe) start the Backbone history.'
+	);
+
+	assert.end();
+} );
+
+test( 'maybeStartHistory (history not yet started) ...', ( assert ) => {
+	const settings = {
+		urlRoot: F.getRandomString()
+	};
+
+	const testee = createTestee( null, settings );
+
+	assert.equal(
+		testee.maybeStartHistory(),
+		true,
+		'... SHOULD return true.'
+	);
+
+	const options = {
+		root: settings.urlRoot,
+		pushState: true,
+		hashChange: false
+	};
+
+	assert.equal(
+		Backbone.history.start.calledWith( options ),
+		true,
+		'... SHOULD start the Backbone history.'
+	);
+
+	// Restore global scope.
+	globalStub.restore();
+
+	assert.end();
+} );
+
+test( 'maybeStartHistory (history already started) ...', ( assert ) => {
+	// Manipulate the Backbone history.
+	Backbone.History.started = true;
+
+	const testee = createTestee();
+
+	assert.equal(
+		testee.maybeStartHistory(),
+		false,
+		'... SHOULD return false.'
+	);
+
+	assert.equal(
+		Backbone.history.start.callCount,
+		0,
+		'... SHOULD NOT start the Backbone history.'
+	);
+
+	// Restore global scope.
+	globalStub.restore();
+
+	assert.end();
+} );
