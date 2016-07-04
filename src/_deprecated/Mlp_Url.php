eeedb1d4af2bac5d07436dadf40f3b54654e9d06
tfrommen <info@tfrommen.de>
2016-07-04 09:03:17 +0200

    Add deprecated URL class. #206

---
+++ src/_deprecated/Mlp_Url.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\EscapedURL;
+
+_deprecated_file(
+	'Mlp_Url',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\EscapedURL'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see EscapedURL}.
+ */
+class Mlp_Url extends EscapedURL {
+
+}
