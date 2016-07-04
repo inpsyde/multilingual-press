b9977b011157bc2e927dfe271d9ed28baa2fa8cd
tfrommen <info@tfrommen.de>
2016-07-04 11:05:29 +0200

    Refactor asset URL interface. #206

---
+++ src/Assets/URL.php
@@ -0,0 +1,23 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Assets;
+
+use Inpsyde\MultilingualPress\Common\Type;
+
+/**
+ * Interface for all asset URL data type implementations, providing a file version.
+ *
+ * @package Inpsyde\MultilingualPress\Assets
+ * @since   3.0.0
+ */
+interface URL extends Type\URL {
+
+	/**
+	 * Returns the file version.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string File version.
+	 */
+	public function get_version();
+}
