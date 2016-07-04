97892880eb930bf8051a7aec1fc697cd174bfc5f
tfrommen <info@tfrommen.de>
2016-07-04 17:43:50 +0200

    Deprecate semantic version number factory class. #206

---
+++ src/_deprecated/Mlp_Semantic_Version_Number_Factory.php
@@ -0,0 +1,35 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\SemanticVersionNumber;
+
+_deprecated_file(
+	'Mlp_Semantic_Version_Number_Factory',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\SemanticVersionNumber'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see SemanticVersionNumber}.
+ */
+class Mlp_Semantic_Version_Number_Factory {
+
+	/**
+	 * Creates a new URL object.
+	 *
+	 * @deprecated 3.0.0 Deprecated in favor of {@see SemanticVersionNumber::create}.
+	 *
+	 * @param mixed $version Version source.
+	 *
+	 * @return SemanticVersionNumber
+	 */
+	public static function create( $version ) {
+
+		_deprecated_function(
+			__METHOD__,
+			'3.0.0',
+			'Inpsyde\MultilingualPress\Common\Type\SemanticVersionNumber::create'
+		);
+
+		return SemanticVersionNumber::create( $version );
+	}
+}
