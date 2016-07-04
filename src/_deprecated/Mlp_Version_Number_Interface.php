a75233c033a7d0c6e31b7df94143ca9cfe4ee6c1
tfrommen <info@tfrommen.de>
2016-07-04 16:06:17 +0200

    Add deprecated version number interface for BC reasons. #206

---
+++ src/_deprecated/Mlp_Version_Number_Interface.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\VersionNumber;
+
+_deprecated_file(
+	'Mlp_Version_Number_Interface',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\VersionNumber'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see VersionNumber}.
+ */
+interface Mlp_Version_Number_Interface extends VersionNumber {
+
+}
