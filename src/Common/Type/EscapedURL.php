950a6c91c9c1e1883d35d6941cffbd95e2a87e57
tfrommen <info@tfrommen.de>
2016-07-04 08:53:05 +0200

    Refactor escaped URL data type class. #206

---
+++ src/Common/Type/EscapedURL.php
@@ -0,0 +1,46 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Common\Type;
+
+/**
+ * Escaped URL data type.
+ *
+ * @package Inpsyde\MultilingualPress\Common\Type
+ * @since   3.0.0
+ */
+class Mlp_Url implements URL {
+
+	/**
+	 * @var string
+	 */
+	private $url = '';
+
+	/**
+	 * Constructor. Sets up the properties.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param mixed $url URL source.
+	 */
+	public function __construct( $url ) {
+
+		if (
+			is_scalar( $url )
+			|| ( is_object( $url ) && method_exists( $url, '__toString' ) )
+		) {
+			$this->url = (string) esc_url( (string) $url );
+		}
+	}
+
+	/**
+	 * Returns the URL string.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+
+		return $this->url;
+	}
+}
