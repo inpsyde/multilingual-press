330cc71f1991ce5d5619df4322d9757ab1d267bf
tfrommen <info@tfrommen.de>
2016-07-04 19:40:43 +0200

    Add deprecated language class for BC reasons. #206

---
+++ src/_deprecated/Mlp_Language.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\AliasAwareLanguage;
+
+_deprecated_file(
+	'Mlp_Language',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\AliasAwareLanguage'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see AliasAwareLanguage}.
+ */
+class Mlp_Language extends AliasAwareLanguage {
+
+}
