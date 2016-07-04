3aead25b5f839cb81942a6a651cc12a1213a3aaf
tfrommen <info@tfrommen.de>
2016-07-04 11:35:30 +0200

    Add deprecated asset URL interface for BC reasons. #206

---
+++ src/_deprecated/Mlp_Asset_Url_Interface.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Assets\URL;
+
+_deprecated_file(
+	'Mlp_Asset_Url_Interface',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Assets\URL'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see URL}.
+ */
+interface Mlp_Asset_Url_Interface extends URL {
+
+}
