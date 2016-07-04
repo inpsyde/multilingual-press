016f34d53f33d32b542b6ee17aa52fae70a099a3
tfrommen <info@tfrommen.de>
2016-07-04 17:35:12 +0200

    Add deprecated semantic version number class for BC reasons. #206

---
+++ src/_deprecated/Mlp_Semantic_Version_Number.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\SemanticVersionNumber;
+
+_deprecated_file(
+	'Mlp_Semantic_Version_Number',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\SemanticVersionNumber'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see SemanticVersionNumber}.
+ */
+class Mlp_Semantic_Version_Number extends SemanticVersionNumber {
+
+}
