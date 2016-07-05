3dbdb8503f2c87cc69e2004a51365f5654cfd3b0
tfrommen <info@tfrommen.de>
2016-07-05 09:26:45 +0200

    Add deprecated translation class for BC reasons. #206

---
+++ src/_deprecated/Mlp_Translation.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\FilterableTranslation;
+
+_deprecated_file(
+	'Mlp_Translation',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\FilterableTranslation'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see FilterableTranslation}.
+ */
+class Mlp_Translation extends FilterableTranslation {
+
+}
