b17d34eb68f953f277da8f12bd4078716cfff6d1
tfrommen <info@tfrommen.de>
2016-07-05 08:19:50 +0200

    Add deprecated translation interface for BC reasons. #206

---
+++ src/_deprecated/Mlp_Translation_Interface.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Common\Type\Translation;
+
+_deprecated_file(
+	'Mlp_Translation_Interface',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Common\Type\Translation'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see Translation}.
+ */
+interface Mlp_Translation_Interface extends Translation {
+
+}
