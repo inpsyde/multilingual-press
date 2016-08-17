3c9ab72ae4ec01e10024465a4f3e2ad011927eb6
tfrommen <info@tfrommen.de>
2016-08-17 10:26:32 +0200

    Introduce Settings data type interface. #206

---
+++ src/Common/Type/Setting.php
@@ -0,0 +1,48 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Common\Type;
+
+/**
+ * Interface for all setting data type implementations.
+ *
+ * @package Inpsyde\MultilingualPress\Common\Type
+ * @since   3.0.0
+ */
+interface Setting {
+
+	/**
+	 * Returns the action name for the setting.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string Action name.
+	 */
+	public function get_action();
+
+	/**
+	 * Returns the nonce name for the setting.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string Nonce name.
+	 */
+	public function get_nonce_name();
+
+	/**
+	 * Returns the title of the setting.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string Setting title.
+	 */
+	public function get_title();
+
+	/**
+	 * Returns the URL to be used in the according form.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return URL URL to submit updates to.
+	 */
+	public function get_url();
+}
