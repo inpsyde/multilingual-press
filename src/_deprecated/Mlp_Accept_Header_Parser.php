ddb64011f20e1fa35443dcc359a1511ae590418b
tfrommen <info@tfrommen.de>
2016-08-17 15:13:36 +0200

    Deprecate old accept header parser class. #206

---
+++ src/_deprecated/Mlp_Accept_Header_Parser.php
@@ -0,0 +1,16 @@
+<?php # -*- coding: utf-8 -*-
+
+use Inpsyde\MultilingualPress\Redirect\LanguageNegotiation\AcceptLanguageParser;
+
+_deprecated_file(
+	'Mlp_Accept_Header_Parser_Interface',
+	'3.0.0',
+	'Inpsyde\MultilingualPress\Redirect\LanguageNegotiation\AcceptLanguageParser'
+);
+
+/**
+ * @deprecated 3.0.0 Deprecated in favor of {@see Parser}.
+ */
+class Mlp_Accept_Header_Parser extends AcceptLanguageParser {
+
+}
