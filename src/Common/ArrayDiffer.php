5fde6df931c76103da4677234b3df30b575fc9f3
tfrommen <info@tfrommen.de>
2016-09-26 17:27:22 +0200

    Introduce array differ interface. #206

---
+++ src/Common/ArrayDiffer.php
@@ -0,0 +1,24 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Common;
+
+/**
+ * Interface for all array differ implementations.
+ *
+ * @package Inpsyde\MultilingualPress\Common
+ * @since   3.0.0
+ */
+interface ArrayDiffer {
+
+	/**
+	 * Compares the given arrays and returns a new array holding the differences only.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param array $a An array of values.
+	 * @param array $b Another array of values.
+	 *
+	 * @return array The array holding the differences only.
+	 */
+	public function diff( array $a, array $b );
+}
