aef053524c0b395411375f3ec7433aee6b475939
tfrommen <info@tfrommen.de>
2016-09-26 16:06:39 +0200

    Introduce factory provider. #206

---
+++ src/Factory/FactoryProvider.php
@@ -0,0 +1,37 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Factory;
+
+use Inpsyde\MultilingualPress\Service\Container;
+use Inpsyde\MultilingualPress\Service\ServiceProvider;
+
+/**
+ * Service provider for all factories.
+ *
+ * @package Inpsyde\MultilingualPress\Factory
+ * @since   3.0.0
+ */
+class FactoryProvider implements ServiceProvider {
+
+	/**
+	 * Registers the provided services on the given container.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param Container $container Container object.
+	 *
+	 * @return void
+	 */
+	public function register( Container $container ) {
+
+		$container->share( 'multilingualpress.error_factory', function () {
+
+			return new Error();
+		} );
+
+		$container->share( 'multilingualpress.type_factory', function () {
+
+			return new TypeFactory();
+		} );
+	}
+}
