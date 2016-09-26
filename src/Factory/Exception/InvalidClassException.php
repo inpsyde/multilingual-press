655b24a35c97b4fab21a5c64205fbb829bc68b55
tfrommen <info@tfrommen.de>
2016-09-26 09:18:04 +0200

    Move factory exception to factory namespace. #206

--- src/Factory/Exception/InvalidClassException.php
+++ src/Factory/Exception/InvalidClassException.php
@@ -1,15 +1,15 @@
 <?php # -*- coding: utf-8 -*-
 
-namespace Inpsyde\MultilingualPress\Exception;
+namespace Inpsyde\MultilingualPress\Factory\Exception;
 
 use Exception;
 
 /**
  * Exception to be thrown when trying to create an object of an invalid class.
  *
- * @package Inpsyde\MultilingualPress\Exception
+ * @package Inpsyde\MultilingualPress\Factory\Exception
  * @since   3.0.0
  */
-class InvalidClassException extends Exception{
+class InvalidClassException extends Exception {
 
 }
