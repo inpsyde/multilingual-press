bf3e64cc11992463c92e4384065cd87fd8d88864
tfrommen <info@tfrommen.de>
2016-09-28 12:41:49 +0200

    Introduce table exception. #206

---
+++ src/Database/Exception/InvalidTableException.php
@@ -0,0 +1,31 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Database\Exception;
+
+use Exception;
+
+/**
+ * Exception to be thrown when an action is to be performed on an invalid table.
+ *
+ * @package Inpsyde\MultilingualPress\Database\Exception
+ * @since   3.0.0
+ */
+class InvalidTableException extends Exception {
+
+	/**
+	 * Returns a new exception object.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $action Optional. The action to be performed. Defaults to 'install'.
+	 *
+	 * @return static Exception object.
+	 */
+	public static function for_action( $action = 'install' ) {
+
+		return new static( sprintf(
+			'Cannot %s. Table invalid.',
+			$action
+		) );
+	}
+}
