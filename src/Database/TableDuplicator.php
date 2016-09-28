ccf0c398b8e2168c30dda320bce84ff9ddb5c4fd
tfrommen <info@tfrommen.de>
2016-09-28 16:14:45 +0200

    Introduce table duplicator. #206

---
+++ src/Database/TableDuplicator.php
@@ -0,0 +1,24 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Database;
+
+/**
+ * Interface for all table duplicator implementations.
+ *
+ * @package Inpsyde\MultilingualPress\Database
+ * @since   3.0.0
+ */
+interface TableDuplicator {
+
+	/**
+	 * Creates a new table that is an exact duplicate of an existing table.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $existing_table Name of the existing table.
+	 * @param string $new_table      Name of the new table.
+	 *
+	 * @return bool Whether or not the table was duplicated successfully.
+	 */
+	public function duplicate_table( $existing_table, $new_table );
+}
---
+++ src/Database/WPDBTableDuplicator.php
@@ -0,0 +1,44 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Database;
+
+use wpdb;
+
+/**
+ * Table duplicator implementation using the WordPress database object.
+ *
+ * @package Inpsyde\MultilingualPress\Database
+ * @since   3.0.0
+ */
+final class WPDBTableDuplicator implements TableDuplicator {
+
+	/**
+	 * @var wpdb
+	 */
+	private $db;
+
+	/**
+	 * Constructor. Sets up the properties.
+	 *
+	 * @since 3.0.0
+	 */
+	public function __construct() {
+
+		$this->db = $GLOBALS['wpdb'];
+	}
+
+	/**
+	 * Creates a new table that is an exact duplicate of an existing table.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $existing_table Name of the existing table.
+	 * @param string $new_table      Name of the new table.
+	 *
+	 * @return bool Whether or not the table was duplicated successfully.
+	 */
+	public function duplicate_table( $existing_table, $new_table ) {
+
+		return (bool) $this->db->query( "CREATE TABLE IF NOT EXISTS $new_table LIKE $existing_table" );
+	}
+}
