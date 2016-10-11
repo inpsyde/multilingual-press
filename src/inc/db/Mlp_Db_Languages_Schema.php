2e4181cb3c5c72fd129def744f860be7a84f5852
tfrommen <info@tfrommen.de>
2016-10-11 09:16:29 +0200

    Fix HTTP code of French (Switzerland). #233

--- src/inc/db/Mlp_Db_Languages_Schema.php
+++ src/inc/db/Mlp_Db_Languages_Schema.php
@@ -761,7 +761,7 @@ class Mlp_Db_Languages_Schema implements Mlp_Db_Schema_Interface {
 				'iso_639_1'    => 'fr',
 				'iso_639_2'    => 'fra',
 				'wp_locale'    => '',
-				'http_name'    => 'fr',
+				'http_name'    => 'fr-CH',
 				'priority'     => 1,
 			),
 			'fy'    => array(
