34c57a88bf42281aecae0418dc179d4bd23ce6a1
tfrommen <info@tfrommen.de>
2016-12-23 09:20:53 +0100

    Version 2.5.0.

--- src/multilingual-press.php
+++ src/multilingual-press.php
@@ -5,7 +5,7 @@
  * Description: Create a fast translation network on WordPress multisite. Run each language in a separate site, and connect the content in a lightweight user interface. Use a customizable widget to link to all sites.
  * Author:      Inpsyde GmbH
  * Author URI:  http://inpsyde.com
- * Version:     2.4.9
+ * Version:     2.5.0
  * Text Domain: multilingual-press
  * Domain Path: /languages
  * License:     GPLv3
