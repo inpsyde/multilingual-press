6cd1831a8bc2c63399d8216ad71c72208c1530b0
tfrommen <info@tfrommen.de>
2017-02-07 19:09:41 +0100

    Introduce integrations. #206

---
+++ src/Integration/Integration.php
@@ -0,0 +1,21 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\MultilingualPress\Integration;
+
+/**
+ * Interface for all integration controllers.
+ *
+ * @package Inpsyde\MultilingualPress\Integration
+ * @since   3.0.0
+ */
+interface Integration {
+
+	/**
+	 * Integrates some (possibly external) service with MultilingualPress.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return bool Whether or not the service was integrated successfully.
+	 */
+	public function integrate();
+}
