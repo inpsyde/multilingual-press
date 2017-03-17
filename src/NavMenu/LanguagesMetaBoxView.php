36e6f32cdec851649e51090e4bc635a15718304c
tfrommen <info@tfrommen.de>
2017-03-17 15:42:19 +0100

    Formatting.

--- src/NavMenu/LanguagesMetaBoxView.php
+++ src/NavMenu/LanguagesMetaBoxView.php
@@ -89,8 +89,8 @@ final class LanguagesMetaBoxView implements MetaBoxView {
 		?>
 		<li>
 			<label class="menu-item-title">
-				<input type="checkbox" value="<?php printf( '%d', $site_id ) ?>" class="menu-item-checkbox">
-				&nbsp;<?php echo esc_attr( $language_name ); ?>
+				<input type="checkbox" value="<?php printf( '%d', $site_id ); ?>" class="menu-item-checkbox">
+				<?php echo esc_attr( $language_name ); ?>
 			</label>
 		</li>
 		<?php
