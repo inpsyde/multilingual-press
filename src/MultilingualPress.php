011de1bc261bc7451154018189f9176df9d52a25
tfrommen <info@tfrommen.de>
2017-03-28 00:02:52 +0200

    Formatting.

--- src/MultilingualPress.php
+++ src/MultilingualPress.php
@@ -3,7 +3,7 @@
 namespace Inpsyde\MultilingualPress;
 
 use Inpsyde\MultilingualPress\Core\Exception\InstanceAlreadyBootstrapped;
-use Inpsyde\MultilingualPress\Installation;
+use Inpsyde\MultilingualPress\Installation\SystemChecker;
 use Inpsyde\MultilingualPress\Module\ActivationAwareModuleServiceProvider;
 use Inpsyde\MultilingualPress\Module\ModuleManager;
 use Inpsyde\MultilingualPress\Module\ModuleServiceProvider;
@@ -63,12 +63,13 @@ final class MultilingualPress {
 	 *
 	 * @since 3.0.0
 	 *
-	 * @param Container                      $container
-	 * @param ServiceProviderCollection|null $service_providers
+	 * @param Container                 $container         Container object.
+	 * @param ServiceProviderCollection $service_providers Service provider collection object.
 	 */
 	public function __construct( Container $container, ServiceProviderCollection $service_providers ) {
 
-		$this->container         = $container;
+		$this->container = $container;
+
 		$this->service_providers = $service_providers;
 	}
 
@@ -87,31 +88,33 @@ final class MultilingualPress {
 			throw new InstanceAlreadyBootstrapped();
 		}
 
-		// first let's register all providers
+		// Register all service providers.
 		$this->service_providers->apply_method( 'register', $this->container );
 
-		// lock the container, nothing can be registered after that
+		// Lock the container. Nothing can be registered after that.
 		$this->container->lock();
 
-		// integrate integration providers
+		// Integrate integration providers.
 		$this->service_providers->filter( function ( ServiceProvider $provider ) {
+
 			return $provider instanceof IntegrationServiceProvider;
 		} )->apply_method( 'integrate', $this->container );
 
-		// if installation check failed, do nothing else
+		// If installation check failed, do nothing else.
 		if ( ! $this->check_installation() ) {
 			return false;
 		}
 
-		// bootstrap all bootstrappable providers
+		// Bootstrap all bootstrappable providers.
 		$this->service_providers->filter( function ( ServiceProvider $provider ) {
+
 			return $provider instanceof BootstrappableServiceProvider;
 		} )->apply_method( 'bootstrap', $this->container );
 
-		// register all modules
+		// Register all modules.
 		$this->register_modules();
 
-		// and bootstrap the container
+		// Bootstrap the container.
 		$this->container->bootstrap();
 
 		/**
@@ -133,7 +136,7 @@ final class MultilingualPress {
 
 		$installation_check = $this->container['multilingualpress.installation_checker']->check();
 
-		return Installation\SystemChecker::PLUGIN_DEACTIVATED !== $installation_check;
+		return SystemChecker::PLUGIN_DEACTIVATED !== $installation_check;
 	}
 
 	/**
@@ -167,9 +170,12 @@ final class MultilingualPress {
 
 		$activation = function ( ModuleServiceProvider $module, ModuleManager $module_manager ) {
 
-			$module->register_module( $module_manager )
-			&& $module instanceof ActivationAwareModuleServiceProvider
-			&& $module->activate();
+			if (
+				$module->register_module( $module_manager )
+				&& $module instanceof ActivationAwareModuleServiceProvider
+			) {
+				$module->activate();
+			}
 		};
 
 		/**
@@ -180,6 +186,7 @@ final class MultilingualPress {
 		do_action( static::ACTION_REGISTER_MODULES );
 
 		$this->service_providers->filter( function ( ServiceProvider $provider ) {
+
 			return $provider instanceof ModuleServiceProvider;
 		} )->apply_callback( $activation, $this->container['multilingualpress.module_manager'] );
 	}
