97582eb3f82c786ea89990c62323fb3fbdb45512
tfrommen <info@tfrommen.de>
2017-04-06 21:41:20 +0200

    Move Translator back where it belongs.

--- src/Translation/Translator/NullTranslator.php
+++ src/Translation/Translator/NullTranslator.php
@@ -4,6 +4,8 @@ declare( strict_types = 1 );
 
 namespace Inpsyde\MultilingualPress\Translation\Translator;
 
+use Inpsyde\MultilingualPress\Translation\Translator;
+
 /**
  * Null translator implementation.
  *
