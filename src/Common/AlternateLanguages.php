12f19b56aabe5696c49e62284d016ee4588e3358
tfrommen <info@tfrommen.de>
2017-05-29 21:02:28 +0200

    Introduce alternate languages data object.

---
+++ src/Common/AlternateLanguages.php
@@ -0,0 +1,81 @@
+<?php # -*- coding: utf-8 -*-
+
+declare( strict_types = 1 );
+
+namespace Inpsyde\MultilingualPress\Common;
+
+use Inpsyde\MultilingualPress\API\Translations;
+
+/**
+ * Alternate languages data object.
+ *
+ * @package Inpsyde\MultilingualPress\Common
+ * @since   3.0.0
+ */
+class AlternateLanguages implements \IteratorAggregate {
+
+	/**
+	 * @var string[]
+	 */
+	private $data;
+
+	/**
+	 * @var Translations
+	 */
+	private $translations;
+
+	/**
+	 * Constructor. Sets up the properties.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param Translations $translations Translations API object.
+	 */
+	public function __construct( Translations $translations ) {
+
+		$this->translations = $translations;
+	}
+
+	/**
+	 * Returns an iterator object for the alternate languages data array.
+	 *
+	 * @sinde 3.0.0
+	 *
+	 * @return \Traversable Iterator object with language HTTP codes as keys and URLs as values.
+	 */
+	public function getIterator() {
+
+		$this->ensure_data();
+
+		return new \ArrayIterator( $this->data );
+	}
+
+	/**
+	 * Takes care that the alternate languages data is available for use.
+	 *
+	 * @return void
+	 */
+	private function ensure_data() {
+
+		if ( isset( $this->data ) ) {
+			return;
+		}
+
+		$translations = $this->translations->get_translations( [
+			'include_base' => true,
+		] );
+
+		$data = [];
+
+		foreach ( $translations as $translation ) {
+			$url = $translation->remote_url();
+			if ( $url ) {
+				$language = $translation->language();
+
+				$data[ $language->name( 'http_code' ) ] = $url;
+			}
+		}
+
+		$this->data = $data;
+	}
+}
