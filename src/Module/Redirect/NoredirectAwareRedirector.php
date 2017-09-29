30879268a35c65d67156b33fba3ef3f687a0da4a
tfrommen <info@tfrommen.de>
2017-09-29 23:54:48 +0200

    Refactor PHP-based redirector.

--- src/Module/Redirect/NoredirectAwareRedirector.php
+++ src/Module/Redirect/NoredirectAwareRedirector.php
@@ -62,7 +62,7 @@ final class NoredirectAwareRedirector implements Redirector {
 	 */
 	public function redirect(): bool {
 
-		$value = (string) $this->request->body_value( NoredirectStorage::KEY, INPUT_GET, FILTER_SANITIZE_STRING );
+		$value = (string) $this->request->body_value( NoredirectPermalinkFilter::QUERY_ARGUMENT, INPUT_GET, FILTER_SANITIZE_STRING );
 		if ( '' !== $value ) {
 			$this->storage->add_language( $value );
 
@@ -77,16 +77,7 @@ final class NoredirectAwareRedirector implements Redirector {
 			return false;
 		}
 
-		/**
-		 * Filters the redirect URL.
-		 *
-		 * @since 3.0.0
-		 *
-		 * @param string         $url             Redirect URL.
-		 * @param RedirectTarget $target          Redirect target object.
-		 * @param int            $current_site_id Current site ID.
-		 */
-		$url = (string) apply_filters( Redirector::FILTER_URL, $target->url(), $target, $current_site_id );
+		$url = $target->url();
 		if ( ! $url ) {
 			return false;
 		}
